06a1f05518d0272f52b81dd489c732ce110353f0
Rafa Sánchez Lucena <sanchez.lucena@gmail.com>
2022-02-05 12:43:52 +0100

    obtiene los datos de aemet

--- clases.js
+++ clases.js
@@ -1,10 +1,10 @@
-export class DatosMeteorologicos {constructor(alt, dmax, dv, fint, hr, idema, lat, lon, prec, ta, tamax, tamin, ubi, vmax, vv) {
+export class DatosMeteorologicos {
+  constructor(alt, dmax, dv, fint, hr, lat, lon, prec, ta, tamax, tamin, ubi, vmax, vv) {
     this.alt = alt;
     this.dmax = dmax;
     this.dv = dv;
     this.fint = fint;
     this.hr = hr;
-    this.idema = idema;
     this.lat = lat;
     this.lon = lon;
     this.prec = prec;
@@ -18,7 +18,7 @@ export class DatosMeteorologicos {constructor(alt, dmax, dv, fint, hr, idema, la
 
   informacion() {
     console.log(
-      `Los datos se obtienen de la población de ${this.ubi}, que está a una altitud de ${this.alt} metros, latitud: ${this.lat} y longitud: ${this.lon}.`
+      `Los datos se obtienen de la población de ${this.ubi}, situada a una altitud de ${this.alt} metros, latitud: ${this.lat} y longitud: ${this.lon}.`
     );
   };
 };
--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 import { DatosMeteorologicos } from "./clases.js";
 import { ordenarArray } from "./funciones.js";
 
+const boton = document.querySelector(".boton");
+
 async function obtenerDatosMeteorologicos(url) {
   try {
+    const acces = await fetch("https://opendata.aemet.es/opendata/api/observacion/convencional/datos/estacion/8059C/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzYW5jaGV6Lmx1Y2VuYUBnbWFpbC5jb20iLCJqdGkiOiIwN2E4NDBiNC1mMDhhLTQ5ODAtOTU3YS1kYjhmYjFmOGE0NjMiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTYzNzMyOTUwNywidXNlcklkIjoiMDdhODQwYjQtZjA4YS00OTgwLTk1N2EtZGI4ZmIxZjhhNDYzIiwicm9sZSI6IiJ9.7AtwvRgQGx_WiwR_IRjfDg43YOF7EVsVWhFnA1YADiQ")
     const request = await fetch(url);
     const data = await request.json();
     // console.log(data);
@@ -13,21 +16,26 @@ async function obtenerDatosMeteorologicos(url) {
       console.log(`Mensaje recibido: ${data.descripcion}`);
     };    
 
-    const dataMeteorologicos = data.map((elemento) => new DatosMeteorologicos(elemento.alt, elemento.dmax, elemento.dv, elemento.fint, elemento.hr, elemento.idema, elemento.lat, elemento.lon, elemento.prec, elemento.ta, elemento.tamax, elemento.tamin, elemento.ubi, elemento.vmax, elemento.vv));
-    
+    const dataMeteorologicos = data.map((element) => new DatosMeteorologicos(element.alt, element.dmax, element.dv, element.fint, element.hr, element.lat, element.lon, element.prec, element.ta, element.tamax, element.tamin, element.ubi, element.vmax, element.vv));
+
+        
     let listaValores = [];
     for (let i = 0; i < dataMeteorologicos.length; i++) {     
       listaValores.push(dataMeteorologicos[i].ta);  
     };
 
     dataMeteorologicos[0].informacion(); 
+
     ordenarArray(listaValores); 
 
   } catch (error) {
     console.log("Lo siento... No se han podido cargar los datos.");
-  };
+    console.log(error);  };
 };
 
 const url = "https://opendata.aemet.es/opendata/sh/4323e5d3"; 
+  
 
-obtenerDatosMeteorologicos(url);
+boton.addEventListener("click", () => {
+  obtenerDatosMeteorologicos(url);
+});
